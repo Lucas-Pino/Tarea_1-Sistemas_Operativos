31bbf455ed3cdfb7dbbf275b10e1ed1db52886f3
Lucas-Pino <53064450+Lucas-Pino@users.noreply.github.com>
2022-10-02 14:32:16 -0300

    hasta aqui funciona con 2 fork() y pipes

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "auxFuncs.h"
 #include "linkedList.h"
 
 #define IS_CHILD 0
@@ -14,7 +13,7 @@
 
 #define DONE 888
 #define WORKING 666
-#define STRLEN
+#define FINISHED -8
 
 
 int main() {
@@ -31,110 +30,118 @@ int main() {
     // EL CAJERO SERA EL PROCESO PRINCIPAL
     pid_t repartidor1;
     pid_t repartidor2;
-    repartidor1=fork();
-    if (repartidor1 < 0){ //Ocurrio un error
-        printf("Fallo el fork child1");
+
+
+
+///////////////// PROCESO PADRE parte 1//////////////////////////
+    //variables auxiliares
+    char line[256];
+    char *token;
+
+    //lista enlazada de productos disponibles, cada nodo contiene una struct 'producto'
+    node *listaProductos = NULL;
+    initList(listaProductos);
+
+    char *pedidos[MAXPEDIDOS];
+
+    //Creando un producto auxiliar
+    producto *tempProd;
+    if ((tempProd = (producto *) malloc(sizeof(producto))) == NULL) {
         return 1;
     }
 
-///////////////////////// PROCESO HIJO 1/////////////////
-    else if(repartidor1==IS_CHILD){
+    //CONTADORES PARA SABER CUANTOS PRODUCTOS HAY EN CADA ARCHIVO
+    size_t pedidosCount = 0;
+
+    //ABRIENDO ARCHIVOS
+    FILE *pedidosFilePtr;
+    FILE *productosFilePtr;
+
+    pedidosFilePtr = fopen(pedidosFile, "r");
+    if (NULL == pedidosFilePtr) {
+        printf("file can't be opened \n");
+    }
+    productosFilePtr = fopen(productosFile, "r");
+    if (NULL == productosFilePtr) {
+        printf("file can't be opened \n");
+    }
+
+//Moviendo los productos a mi lista enlazada de productos
+    while (fgets(line, sizeof(line), productosFilePtr)) {
+
+        //NAME
+        token = strtok(line, ",");
+        tempProd->name = malloc(strlen(token) + 1);
+        strcpy(tempProd->name, token);
+
+        //PRECIO
+        token = strtok(NULL, ",");
+        tempProd->price = atoi(token);
+
+        //ID
+        token = strtok(NULL, ",");
+        tempProd->id = atoi(token);
+
+        //TIME
+        token = strtok(NULL, ",");
+        tempProd->time = atoi(token);
+
+        push(&listaProductos, tempProd);
+
+    }
+///// CREANDO LOS FORK AQUI POR QUE ASI TODOS LOS PROCESOS TIENEN ACCESO A LA INFORMACION DE LOS PRODUCTOS (pero no a la lista de pedidos)
 
 
-        size_t pedidosCount;
-
+////// P R O C E S O    H I J O  1 /////////////////////////////////////////////////////////////////////////////////// 1
+    repartidor1=fork();
+    sleep(1);
+    if (repartidor1 < 0){ //Ocurrio un error
+        printf("Fallo el fork child1");
+        return 1111;
+    }else if(repartidor1 == IS_CHILD){
+        sleep(1);
         int pedido;
+//        while(pedido!=FINISHED){
+//
+//        }
         read(data1[0], &pedido, sizeof(int));
-        printf("\n child 1 WAITING for %d \n", pedido);
+        printf("\n child 1 WAITING for pedido %d \n", pedido);
         sleep(1);
         printf("\n child 1 FINISHED pedido %d \n", pedido);
         pedido=DONE;
         sleep(1);
-        write(data1[1], DONE, sizeof(int ));
+        write(data1[1], &pedido, sizeof(int ));
     }
 
-
-    else{
-        repartidor2=fork();
-        if (repartidor2 < 0){ //Ocurrio un error
+    else {
+////// P R O C E S O    H I J O  2 /////////////////////////////////////////////////////////////////////////////////// 2
+        repartidor2 = fork();
+        sleep(1);
+        if (repartidor2 < 0) { //Ocurrio un error
             printf("Fallo el fork child 2");
-            return 1;
-        }
-////////////////////////// PROCESO HIJO 2/////////////////
-        else if (repartidor2==IS_CHILD){
+            return 2222;
+        } else if (repartidor2 == IS_CHILD) {
+            sleep(1);
             int pedido;
             read(data2[0], &pedido, sizeof(int));
-            printf("\n child 2 waiting for %d \n", pedido);
+            printf("\n child 2 WAITING for %d \n", pedido);
             sleep(2);
             printf("\n child 2 FINISHED pedido %d \n", pedido);
-            pedido=DONE;
+            pedido = DONE;
             sleep(2);
-            write(data2[1], &pedido, sizeof(int ));
-        } else {
-///////////////// PROCESO PADRE//////////////////////////
+            write(data2[1], &pedido, sizeof(int));
+        }
 
-            //variables auxiliares
-            char line[256];
-            char *token;
-
-            //lista enlazada de productos disponibles, cada nodo contiene una struct 'producto'
-            node *listaProductos = NULL;
-            initList(listaProductos);
-
-            char *pedidos[MAXPEDIDOS];
-
-            //Creando un producto auxiliar
-            producto *tempProd;
-            if ((tempProd = (producto *) malloc(sizeof(producto))) == NULL) {
-                return 1;
-            }
-
-            //CONTADORES PARA SABER CUANTOS PRODUCTOS HAY EN CADA ARCHIVO
-            size_t pedidosCount = 0;
-
-            //ABRIENDO ARCHIVOS
-            FILE *pedidosFilePtr;
-            FILE *productosFilePtr;
-
-            pedidosFilePtr = fopen(pedidosFile, "r");
-            if (NULL == pedidosFilePtr) {
-                printf("file can't be opened \n");
-            }
-            productosFilePtr = fopen(productosFile, "r");
-            if (NULL == productosFilePtr) {
-                printf("file can't be opened \n");
-            }
-
-//Moviendo los productos a mi lista enlazada de productos
-            while (fgets(line, sizeof(line), productosFilePtr)) {
-
-                //NAME
-                token = strtok(line, ",");
-                tempProd->name = malloc(strlen(token) + 1);
-                strcpy(tempProd->name, token);
-
-                //PRECIO
-                token = strtok(NULL, ",");
-                tempProd->price = atoi(token);
-
-                //ID
-                token = strtok(NULL, ",");
-                tempProd->id = atoi(token);
-
-                //TIME
-                token = strtok(NULL, ",");
-                tempProd->time = atoi(token);
-
-                push(&listaProductos, tempProd);
-
-            }
+        else {
+////CONTINUACION PROCESO PADRE ///////////////////////////////////////////////////////////////////////////////////
 //    Agregando pedidos a lista de pedidos
             fgets(line, sizeof(line), pedidosFilePtr);
             token = strtok(line, ",");
-            for (pedidosCount; token; pedidosCount++) {
+            while (token) {
                 pedidos[pedidosCount] = malloc(strlen(token) + 1);
                 strcpy(pedidos[pedidosCount], token);
                 token = strtok(NULL, ",");
+                pedidosCount++;
             }
 
 //    Recorriendo la lista enlazada de productos disponibles
@@ -161,65 +168,59 @@ int main() {
             }
             printf("\n");
 
-            //TESTING THE PIPES
-            int x=888;
-            int x1=999;
-            //enviando los pedidos por el pipe
+//    //TESTING THE PIPES
+//    int x=888;
+//    int x1=999;
+//    //enviando los pedidos por el pipe
+//
+//    write(data1[1], &x, sizeof(int));
+//    write(data1[1], &x1, sizeof(int));
 
-            write(data1[1], &x, sizeof(int));
-            write(data1[1], &x1, sizeof(int));
-
-            int p1_status=DONE;
-            int p2_status=DONE;
+            int p1_status = DONE;
+            int p2_status = DONE;
             int prod1;
             int prod2;
-            int finished=0;
-            int pedidosDone=0;
+            int finished = 0;
+            int pedidosDone = 0;
 
 
-            char temp[256];
-            strcpy(temp,pedidos[2]);
+//            prod1 = getId(listaProductos, pedidos[2]);
+//            printf("get id %d", prod1);
 
-            prod1=getId(listaProductos,pedidos[2]);
-            printf("get id %d", prod1);
-
-            write(data1[1], &prod1, sizeof(int));
+//            write(data1[1], &prod1, sizeof(int));
 
             //SCHEDULER PART
-            for (int i = 0; i < pedidosCount; i++) {
-                printf("%s\n", pedidos[i]);
-//                if(pedidos)
-            }
-            while(pedidosDone!=pedidosCount){
+            while (pedidosDone != pedidosCount) {
+                printf("PedidosDone %d | PedidosCount %d\n",pedidosDone,pedidosCount);
 //                read(data1[0], &p1_status, sizeof(int));
-                if(p1_status==DONE){
-                    prod1=getId(listaProductos,getNextPedido(pedidos,MAXPEDIDOS));
+                if (p1_status == DONE) {
+                    prod1 = getId(listaProductos, getNextPedido(pedidos, MAXPEDIDOS));
                     printf("sent prod %d to child 1 \n", prod1);
                     write(data1[1], &prod1, sizeof(int));
                     pedidosDone++;
-                    p1_status=WORKING;
+                    p1_status = WORKING;
                 }
 //                read(data1[0], &p2_status, sizeof(int));
-                if(p2_status==DONE){
-                    prod2=getId(listaProductos,getNextPedido(pedidos,MAXPEDIDOS));
+                if (p2_status == DONE) {
+                    prod2 = getId(listaProductos, getNextPedido(pedidos, MAXPEDIDOS));
                     printf("sent prod %d to child 2 \n", prod2);
                     write(data2[1], &prod2, sizeof(int));
                     pedidosDone++;
-                    p2_status=WORKING;
+                    p2_status = WORKING;
                 }
                 sleep(1);
                 read(data1[0], &p1_status, sizeof(int));
 
                 read(data2[0], &p2_status, sizeof(int));
                 sleep(1);
+                printf("PedidosDone %d | PedidosCount %d\n",pedidosDone,pedidosCount);
             }
 
 
             printf("DONE ALL \n");
 
 
-
-
+            wait(NULL);
             wait(NULL);
             close(data1[0]);
             close(data1[1]);
@@ -228,9 +229,11 @@ int main() {
             free(listaProductos);
             // free(tempNode);
             // free(pedidos);
+
             return 0;
+
         }
     }
 ///////////FIN DEL PROCESO PADRE///////////////
-    wait(NULL);
+
 }
